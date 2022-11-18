186360d3df966869cb91ee724a4ae88027d19b1c
stefanmarek <UAAE1Y6@post.at>
2022-11-18 22:28:55 +0100

    code fragment parts

---
+++ nicoBot/src/components/CodeFragment.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="draggable command" :key="id">
+                    <div class="name">{{ fragment.name }}</div>
+                    <Condition :conditions="fragment.conditions" />
+                    <div class="function">{{ CodeFunction[fragment.function] }}</div>
+                    <div class="value">{{ fragment.numVal }}{{ fragment.strValue }} {{ fragment.direction ? Direction[fragment.direction] : "" }}</div>
+                    <div v-if="fragment.subFunctions" v-for="(fr, ix) in fragment.subFunctions">
+                        <CodeFragment :fragment="fr" :id="ix * 100" />
+                    </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { UICodeFragment, CodeFunction, Direction } from '@/UICodeFragment';
+import Condition from "./Condition.vue";
+
+interface CodeFragmentProps {
+    fragment: UICodeFragment;
+    id: number;
+};
+
+const props = defineProps<CodeFragmentProps>()
+</script>
+<style scoped>
+.command {
+    padding: 5px;
+    border: 1px solid silver;
+    display:block;
+    width: 100%;
+}
+.command > div {
+    padding: 5px;
+    display: inline-block;
+}
+</style>    
--- nicoBot/src/components/Editor.vue
+++ nicoBot/src/components/Editor.vue
@@ -4,12 +4,7 @@
 
         <Sortable :list="elements" item-key="id" tag="div" :options="options">
             <template #item="{ element, index }">
-                <div class="draggable command" :key="element.id">
-                    <div class="name">{{ element.name }}</div>
-                    <Condition :conditions="element.conditions" />
-                    <div class="function">{{ CodeFunction[element.function] }}</div>
-                    <div class="value">{{ element.numVal }}{{ element.strVal }} {{ Direction[element.direction] }}</div>
-                </div>
+                <CodeFragment :fragment="element" :id="index * 100" />
             </template>
         </Sortable>
     </div>
@@ -19,6 +14,7 @@
 //
 import { UICodeFragment, CodeFunction, Direction, Operator } from "@/UICodeFragment";
 import { Sortable } from "sortablejs-vue3";
+import CodeFragment from "./CodeFragment.vue";
 import Condition from "./Condition.vue";
 
 let hello = "Hallo Nico!";
@@ -29,7 +25,12 @@ let elements: UICodeFragment[] = [
     { function: CodeFunction.go, numVal: 30 },
     { function: CodeFunction.turn, direction: Direction.left },
     { function: CodeFunction.go, numVal: 30 },
-    { function: CodeFunction.go, numVal: -60, conditions: [{ left: "x", operator: Operator.equals, right: "2"}] },
+    { function: CodeFunction.go, numVal: -60, conditions: [{ left: "x", operator: Operator.equals, right: "2"}],
+    subFunctions: [
+        { function: CodeFunction.turn, direction: Direction.left },
+        { function: CodeFunction.go, numVal: 30 },
+    ]
+},
     
 ];
 
@@ -37,14 +38,4 @@ let elements: UICodeFragment[] = [
 </script>
 
 <style scoped>
-.command {
-    padding: 5px;
-    border: 1px solid silver;
-    display:block;
-    width: 100%;
-}
-.command > div {
-    padding: 5px;
-    display: inline-block;
-}
 </style>
